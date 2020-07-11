e2f9979d088d9db8c573e51a99ec5aca3d6c94eb
Yulun Wu <yulun_wu@berkeley.edu>
2020-07-10 17:57:15 -0700

    finished test

--- tests/testthat/test-survival.R
+++ tests/testthat/test-survival.R
@@ -10,13 +10,13 @@ library(tmle3trans)
 simulate_data <- function(n_sim = 2e2) {
   D <- DAG.empty()
   D <- D +
+    node("S", distr = "rbinom", size = 1, prob = .6) +
     node("W1", distr = "rbinom", size = 1, prob = .5) +
-    node("W", distr = "runif", min = 0, max = 1.5) +
-    node("S", distr = "rbinom", size = 1, prob = .15 + .5 * as.numeric(W > .75)) +
-    node("Trexp", distr = "rexp", rate = 1 + .7 * W^2 - .8 * S) +
+    node("W", distr = "runif", min = .1 - .1 * S, max = 1.6 - .1 * S) +
+    node("Trexp", distr = "rexp", rate = .2 + .7 * W^2) +
     node("Cweib", distr = "rweibull", shape = 1 + .5 * W, scale = 75) +
-    node("T", distr = "rconst", const = round(Trexp * 2)) +
-    node("C", distr = "rconst", const = round(Cweib * 2)) +
+    node("T", distr = "rconst", const = round(Trexp * 2) + 1) +
+    node("C", distr = "rconst", const = round(Cweib * 2) + 1) +
     # Observed random variable (follow-up time):
     node("T.tilde", distr = "rconst", const = ifelse(T <= C, T, C)) +
     # Observed random variable (censoring indicator, 1 - failure event, 0 - censored):
@@ -27,37 +27,37 @@ simulate_data <- function(n_sim = 2e2) {
   Wname <- grep("W", colnames(dat), value = TRUE)
   dat <- dat[, c("ID", Wname, "S", "T.tilde", "Delta")]
   # input: scalar q, W vector. computes for all W, the S(q|S,W)
-  true_surv <- function(q, W, S) sapply(W, function(w) {
-    1 - pexp(q, rate = 1 + .7 * w^2 - .8 * S)
+  true_surv_w <- function(q, W) sapply(W, function(w) {
+    1 - pexp(q, rate = .2 + .7 * w^2)
   })
   # input: vector q. mean(S(q|S=s,W)), average out W. loop over q
-  true_surv <- function(q_grid, surv_fn, s) {
-    W_grid <- seq(0, 1.5, .01)
+  true_surv_s <- function(q_grid, surv_fn, s) {
+    W_grid <- seq(.1 - .1 * s, 1.6 - .1 * s, .01)
     survout <- numeric()
-    for (q in q_grid) survout <- c(survout, mean(surv_fn(q = q / 2, W = W_grid, S = s)))
+    for (q in q_grid) survout <- c(survout, mean(surv_fn(q = q / 2, W = W_grid)))
     return(survout)
   }
-  truth_surv0 <- function(q) true_surv(q_grid = q, surv_fn = true_surv_one, s = 0)
-  return(list(dat = dat, true_surv0 = truth_surv0))
+  truth_surv1 <- function(q) true_surv_s(q_grid = q, surv_fn = true_surv_w, s = 1)
+  truth_surv0 <- function(q) true_surv_s(q_grid = q, surv_fn = true_surv_w, s = 0)
+  return(list(dat = dat, true_surv1 = truth_surv1, true_surv0 = truth_surv0))
 }
 
-set.seed(1234)
+set.seed(4321)
 n_sim <- 1e2
 simulated <- simulate_data(n_sim = n_sim)
 df <- simulated$dat
-true_surv <- simulated$true_surv0
 
 # TODO: check
 while (all(df$Delta == 1)) {
   simulated <- simulate_data(n_sim = n_sim)
   df <- simulated$dat
-  true_surv <- simulated$true_surv0
 }
 
 ###########
 # tlverse #
 ###########
 tmax <- max(df$T.tilde)
+k_grid <- 1:tmax
 all_times <- lapply(seq_len(tmax), function(t) df_time(df, t))
 
 df_long <- rbindlist(all_times)
@@ -72,7 +72,7 @@ lrnr_gam <- make_learner(Lrnr_gam)
 #lrnr_earth <- make_learner(Lrnr_earth)
 #lrnr_sl <- Lrnr_sl$new(learners = list(lrnr_mean, lrnr_glm, lrnr_gam, lrnr_earth))
 lrnr_sl <- Lrnr_sl$new(learners = list(lrnr_mean, lrnr_glm, lrnr_gam))
-learner_list <- list(S = lrnr_sl, A = lrnr_sl, F = lrnr_sl, C = lrnr_sl)
+learner_list <- list(S = lrnr_sl, F = lrnr_sl, C = lrnr_sl)
 
 ### tmle3 ###
 # TODO: check
@@ -107,5 +107,12 @@ tmle_fit_manual <- fit_tmle3(
   targeted_likelihood$updater
 )
 
-tmle_fit_manual
+psi0_0 <- tmle_fit_manual$summary$init_est
+psi0_n <- tmle_fit_manual$summary$tmle_est
+
+# TODO: check
+t_surv1 <- simulated$true_surv1(k_grid - 1)
+t_surv0 <- simulated$true_surv0(k_grid - 1)
+l2_diff(psi0_0, t_surv0)
+l2_diff(psi0_n, t_surv0)
 
