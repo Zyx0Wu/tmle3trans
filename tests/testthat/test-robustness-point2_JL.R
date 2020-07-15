51283aab6b3763936a551496e1f458be5ab2a26e
Levy <levyj7@nala.roche.com>
2020-07-14 23:46:49 -0700

    now has the stabilized weights iptw estimator with proper IC inference which is more efficient than iptw but way worse than tmle and doesn't cover nominally--worsening with n.

--- tests/testthat/test-robustness-point2_JL.R
+++ tests/testthat/test-robustness-point2_JL.R
@@ -8,6 +8,9 @@ library(assertthat)
 library(data.table)
 library(future)
 library(dplyr)
+library(foreach)
+library(parallel)
+library(ggplot2)
 
 # setup data for test
 set.seed(1234)
@@ -29,8 +32,29 @@ popS0 =  filter(pop, S==0)
 popS1 =  filter(pop, S==1)
 Psi0 = mean(popS0$Y)
 mean(popS1$Y)
+Ypop = pop$Y
+Spop = pop$S
 
+### fit ###
+popW = select(pop, node_list$W)
+node_list <- list(W = c("W1", "W2", "W3"), S = "S", Y = "Y")
+W <- select(pop, node_list$W)
+
+### 1. Naive ###
+# note: not Plug-In
+bias = T
+Qpop = mutate(W, Q = -1 - .5 * W1 +
+                .8 * W2 + .2 * W3 + bias * n^(-0.3))$Q
+Qpop
+PSpop = mutate(W, g=prob_clip(expit(1.4 - 0.6 * W1 - 2 * W2 +
+                                    0.7 * W3 + bias * n^(-0.3))))$g
+PSpop
 Psi0
+
+Dpop = Spop*(1-PSpop)/PSpop/mean(1-Spop)*(Ypop - Qpop)+
+  (1-Spop)/mean(1-Spop)*(Qpop-mean(Qpop[Spop==0]))
+
+se0 = sd(Dpop)
 
 ###
 ###
@@ -39,10 +63,10 @@ Psi0
 ###
 ###
 
-n=1000
-biasS = T
-biasY = T
-simJL = function(n, biasS, biasY) {
+simJL = function(n, biasS, biasY, wt=TRUE) {
+  # n=100
+  # biasS=biasY=T
+  # wt=T
   data = gendata_trans(n)
   node_list <- list(W = c("W1", "W2", "W3"), S = "S", Y = "Y")
 
@@ -50,12 +74,6 @@ simJL = function(n, biasS, biasY) {
 
   data0 <- filter(data, S==0)
   data1 <- filter(data, S==1)
-
-  ### observed mean ###
-  # YS0 <- data0[ ,colnames(data0) %in% node_list$Y, with=FALSE]
-  # mean <- mean(as.matrix(YS0))
-  # mean
-  # sd <- sd(as.matrix(YS0)) / sqrt(length(as.matrix(YS0)))
 
   ### fit ###
   WS1 = select(data1, node_list$W)
@@ -68,10 +86,13 @@ simJL = function(n, biasS, biasY) {
 
   ### 1. Naive ###
   # note: not Plug-In
-  IS0EYW <- apply(WS0, 1, y_dens, n=1000, bias = biasY)
+  Q = apply(W, 1, y_dens, bias = biasY, n = n)
 
-  naive_psi <- mean(IS0EYW)
+  naive_psi <- mean(QS0)
   naive_psi
+
+  S = data$S
+  Y = data$Y
 
   ###
   ###
@@ -80,65 +101,98 @@ simJL = function(n, biasS, biasY) {
   ###
 
   ### 2. IPTW ###
-  pS1W <- apply(W, 1, s_dens,n=1000, bias=T)
-  IS1 <- data$S == 1
+  pS1W <- apply(W, 1, s_dens,n=n, bias=T)
 
   pS0W <- 1 - pS1W
   pS0 <- mean(data$S==0)
 
-  iptw_psi <- mean(IS1/pS1W * pS0W/pS0 * data$Y)
-  iptw_psi
+  H1 = S*pS0W/(pS0*pS1W)
 
-  iptw_se <- sd(IS1/pS1W * pS0W/pS0 * data$Y)/sqrt(n)
-  iptw_CI95 <- sprintf("(%f, %f)", iptw_psi - 1.96*iptw_se, iptw_psi + 1.96*iptw_se)
+  iptw_psi_init <- mean(H1*Y)
+  iptw_psi = iptw_psi_init/(mean(H1))
+  iptw_se <- sd(H1*(Y-iptw_psi_init))/sqrt(n)
+  iptw_CI95 <- c(iptw_psi, iptw_psi - 1.96*iptw_se, iptw_psi + 1.96*iptw_se)
   iptw_CI95
 
   ### 3. TML ###
-  tmle_spec <- tmle_AOT(1, 0)
 
-  # define data
-  tmle_task <- tmle_spec$make_tmle_task(data, node_list)
+  tmle_psi_init = mean(Q[S==0])
 
-  # define likelihood
-  g_dens <- function(task) apply(task$get_node("covariates"),
-                                 1, s_dens,n, bias=biasS)
-  Q_mean <- function(task) apply(task$get_node("covariates"),
-                                 1, y_dens, n, bias=biasY)
-  #g_dens <- function(task) apply(task$get_tmle_node("W"), 1, s_dens, bias=TRUE)
-  #Q_mean <- function(task) apply(task$get_tmle_node("W"), 1, y_dens, bias=FALSE)
+  if (wt) {
+    tmlefit = glm(Y[S==1]~1+offset(Q[S==1]),
+                  family = gaussian, weights=H1[S==1])
+    Qstar = Q+ tmlefit$coefficients
+  } else {
+    tmlefit = glm(Y[S==1]~-1+H1[S==1] + offset(Q[S==1]),
+                  family = gaussian)
+    Qstar = tmlefit$coefficients*pS0W/(mean(1-S)*pS1W)+Q
+  }
 
-  factor_list <- list(
-    define_lf(LF_emp, "W"),
-    define_lf(LF_known, "S", density_fun = g_dens),
-    define_lf(LF_known, "Y", mean_fun = Q_mean, type = "mean")
-  )
+  D = S*pS0W/(mean(1-S)*pS1W)*(Y-Qstar) +
+    (1-S)/mean(1-S)*(Qstar-mean(Qstar[S==0]))
 
-  # estimate likelihood
-  initial_likelihood <- Likelihood$new(factor_list)$train(tmle_task)
+  mean(D)
 
-  # define update method (submodel + loss function)
-  updater <- tmle3_Update$new()
-  targeted_likelihood <- Targeted_Likelihood$new(initial_likelihood, updater)
+  tmle_psi = mean(Qstar[S==0])
+  tmle_psi_init
+  tmle_psi
+  tmle_se <- sd(D)/sqrt(n)
 
-  # define parameter
-  tmle_param <- tmle_spec$make_params(tmle_task, targeted_likelihood)
+  tmle_CI95 <- c(tmle_psi, tmle_psi - 1.96*tmle_se, tmle_psi + 1.96*tmle_se)
+  tmle_CI95
 
-  # fit
-  tmle_fit <- fit_tmle3(tmle_task, targeted_likelihood, tmle_param, updater)
-
-  # extract results
-  tmle_summary <- tmle_fit$summary
-  sl_psi <- tmle_summary$init_est
-  tmle_psi <- tmle_summary$tmle_est
-  tmle_se <- tmle_summary$se
-  tmle_epsilon <- updater$epsilons[[1]]$Y
-
-  tmle_CI95 <- sprintf("(%f, %f)", tmle_psi - 1.96*tmle_se, tmle_psi + 1.96*tmle_se)
-
-  return(list(tmle = c(tmle_psi, tmle_CI95),
-              iptw = c(iptw_psi, iptw_CI95),
+  return(list(tmle =  tmle_CI95,
+              iptw = iptw_CI95,
+              tmle_psi_init = tmle_psi_init,
               naive_psi = naive_psi))
 }
 
-simJL(1000, T,T)
+simJL(1000,T,T,F)
+cl = makeCluster(8)
+
+n=1000
+B=5000
+res = foreach(i=1:B, .errorhandling = "remove") %do%
+  simJL(n,T,T,T)
+
+getres = function(res) {
+
+  res = do.call(rbind,lapply(res, function(L) unlist(L)))
+  res = data.frame(res)
+  head(res)
+  tmlecov = mean(res$tmle2 <= Psi0 & res$tmle3 >= Psi0)
+  tmleeff = mean((res$tmle3- res$tmle2)/(2*1.96)/se0_n)
+  iptwcov = mean(res$iptw2 <= Psi0 & res$iptw3 >= Psi0)
+  iptweff = mean((res$iptw3- res$iptw2)/(2*1.96)/se0_n)
+
+  coverage = c(tmle = tmlecov,iptw = iptwcov)
+  efficiency = c(tmle = tmleeff, iptw = iptweff)
+  cov_eff = rbind(coverage, efficiency)
+  cov_eff
+
+  mse = apply(res[,c(1,4,7,8)], 2, function(x) mean((x-Psi0)^2))
+  mse
+
+  ests = unlist(res[,c(1,4,7,8)])
+  names(ests) = NULL
+  ests
+  type = c(rep("tmle",n),
+           rep("iptw",n), rep("tmle_init",n),
+           rep("naive_psi", n))
+  type
+  plotdf = data.frame(ests = ests, type = type)
+  return(list(mse = mse, cov_eff = cov_eff, plotdf = plotdf))
+}
+
+se0_n = se0/sqrt(n)
 Psi0
+results = getres(res)
+
+results[1:2]
+
+sampling_dists = ggplot(results$plotdf, aes(x=ests, fill = type))+
+  geom_density(alpha=.4)+geom_vline(xintercept=Psi0)+
+  xlim(-1.2,-.1)
+
+sampling_dists
+
